7079498b653ace8e23f2f5314bccb241e0ea95f9
Taeeun Kim <taeeun.kim@outlook.com>
2020-08-24 01:24:01 +0200

    Basic fuction

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Taeeun Kim on 23.08.20.
+//  Copyright © 2020 Taeeun Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Max! How are you doing?")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
