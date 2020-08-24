ea2a5d43d9599748e0dee839021820c5b0df8f65
Taeeun Kim <taeeun.kim@outlook.com>
2020-08-24 03:04:00 +0200

    Learned Basic fuction of Swift

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Max! How are you doing?")
+        HStack(){
+            ForEach(0..<4){ index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+        
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack() {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
