bd943d79e8e599c717518bf5d65139a30d7ae1a7
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-05 15:49:33 +0200

    Core Data

---
+++ CoreDataDemo/CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Taeeun Kim on 05.04.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
