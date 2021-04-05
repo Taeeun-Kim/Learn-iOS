2e1fb405a1aaaeb745cabafd54849df33cc16176
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-05 21:39:15 +0200

    AnyView

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,35 +7,23 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-    
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
-    
+    @Environment(\.horizontalSizeClass) var sizeClass
+
     var body: some View {
-        VStack{
-            PushButton(title: "RememberMe", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+        if sizeClass == .compact {
+            return AnyView(HStack {
+                Text("Active size class:")
+                Text("COMPACT")
+            }
+            .font(.largeTitle))
+        } else {
+            return AnyView(HStack {
+                Text("Active size class:")
+                Text("REGULAR")
+            }
+            .font(.largeTitle))
         }
-        
     }
 }
 
@@ -44,3 +32,44 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//import SwiftUI
+//
+//struct PushButton: View {
+//    let title: String
+//    @Binding var isOn: Bool
+//
+//    var onColors = [Color.red, Color.yellow]
+//    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+//
+//    var body: some View {
+//        Button(title) {
+//            self.isOn.toggle()
+//        }
+//        .padding()
+//        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+//        .shadow(radius: isOn ? 0 : 5)
+//    }
+//
+//}
+//
+//struct ContentView: View {
+//    @State private var rememberMe = false
+//
+//    var body: some View {
+//        VStack{
+//            PushButton(title: "RememberMe", isOn: $rememberMe)
+//            Text(rememberMe ? "On" : "Off")
+//        }
+//
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
