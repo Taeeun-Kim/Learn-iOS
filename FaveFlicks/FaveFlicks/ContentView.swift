cd07e6a03905313ee90fd0ab024ef8516caa51f5
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-06 18:39:00 +0200

    Update

---
+++ FaveFlicks/FaveFlicks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaveFlicks
+//
+//  Created by Taeeun Kim on 05.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
