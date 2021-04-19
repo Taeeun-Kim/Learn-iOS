eb388004cc88b85cb5836d493cd03dd3b01531ac
Taeeun Kim <taeeun.kim@outlook.com>
2021-04-19 23:18:43 +0200

    Update comments

--- Enroute L11/Enroute/FilterFlights.swift
+++ Enroute L11/Enroute/FilterFlights.swift
@@ -12,21 +12,38 @@ struct FilterFlights: View {
     @ObservedObject var allAirports = Airports.all
     @ObservedObject var allAirlines = Airlines.all
 
-    @Binding var flightSearch: FlightSearch
+    // 아직 var flightSearch와 var isPresented들은 initialize가 안됨 -> init으로 초기화 ㄱ
+    // 밑에 두개는 아직 논 언더바 버전 = wrapped value of this struct
+    // 달러가 붙으면 projected value of this struct
+    // init 되기전에는 사용 불가
+    @Binding var flightSearch: FlightSearch // FlightSearch는 struct지만 var로 변수로 받아버림
     @Binding var isPresented: Bool // @State private var showFilter = false <-를 메인뷰에서 썻으니, 여기에서도 같은 변수를 써야하니 @Binding으로 선언
     
     @State private var draft: FlightSearch
     // 바인딩된 거 말고, 이걸로 변경된거를 전역변수(바인딩)인 self.flightSearch = self.draft에 이렇게 넣어주기, 변경 완료!
     
-    init(flightSearch: Binding<FlightSearch>, isPresented: Binding<Bool>) {
+    // Binding은 제네릭, don't care about type, _도 같은 의미, 그러니깐 바인딩은 언더스코어로 선언!!!
+    init(flightSearch: Binding<FlightSearch>, isPresented: Binding<Bool>) { // 아하, Binding 된 거를 이렇게 init 시켜줌!
         _flightSearch = flightSearch
         _isPresented = isPresented
         _draft = State(wrappedValue: flightSearch.wrappedValue)
+//        self.draft = flightSearch self.draft는 FlightSearch가 밸류이고, flightSearch의 밸류는 Binding<FlightSearch>라서 안됨
+//        Cannot assign value of type 'Binding<FlightSearch>' to type 'FlightSearch'
     }
+    
+    /*
+    언더스코어 예제:
+    func myFunc(label name: Int) // call it like myFunc(label: 3)
+    func myFunc(name: Int) // call it like myFunc(name: 3)
+    func myFunc(_ name: Int) // call it like myFunc(3)
+    */
     
     var body: some View {
         NavigationView {
             Form {
+                // 대부분 String, selection, content 적혀있는 Picker만 사용함
+                // selection: what I want to change -> $draft.destination
+                // Picker의 content는 대부분 ForEach 사용
                 Picker("Destination", selection: $draft.destination) {
                     ForEach(allAirports.codes, id: \.self) { airport in
                         Text("\(self.allAirports[airport]?.friendlyName ?? airport)").tag(airport)
