8c3d800138e3f57d08a4c415e5a571286a924d02
Taeeun Kim <taeeun.kim@outlook.com>
2021-06-28 11:53:21 +0200

    Update HTTPClient

---
+++ MoviesApp/MoviesApp/Services/HTTPClient.swift
@@ -0,0 +1,41 @@
+//
+//  HTTPClient.swift
+//  MoviesApp
+//
+//  Created by Taeeun Kim on 28.06.21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case noData
+    case decodingError
+}
+
+class HTTPClient {
+    
+    // @frozen enum Result<Success, Failure> where Failure : Error
+    func getMoviesBy(search: String, completion: @escaping (Result<[Movie]?, NetworkError>) -> Void) {
+        
+        guard let url = URL(string: "http://www.omdbapi.com/?s=\(search)&apikey=\(Constants.API_KEY)") else {
+            return completion(.failure(.badURL))
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+            
+            guard let movieResponse = try? JSONDecoder().decode(MovieResponse.self, from: data)
+            else {
+                return completion(.failure(.decodingError))
+            }
+            
+            completion(.success(movieResponse.movies))
+        }
+        .resume()
+    }
+}
