9c6e81aa916f7ae75e2038116fdfabd5e907c1ae
Taeeun Kim <taeeun.kim@outlook.com>
2021-08-19 23:38:26 +0200

    Set AuthManager, update WelcomeVC, TabBarVC

--- UIKit Spotify Clone/UIKit Spotify Clone/Managers/AuthManager.swift
+++ UIKit Spotify Clone/UIKit Spotify Clone/Managers/AuthManager.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+final class AuthManager {
+    // AuthManager().isSignedIn으로도 사용 가능
+    // 하지만 "'AuthManager' initializer is inaccessible due to 'private' protection level"
+    // 그렇기에 shared를 이용해서 접근
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    var isSignedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String?  {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool {
+        return false
+    }
+}
