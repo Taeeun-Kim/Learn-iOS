3e3a9c23973739bd2df5c6104c6af8ededbbb48b
Taeeun Kim <taeeun.kim@outlook.com>
2021-09-03 22:49:15 +0200

    Update flipCard func

--- Concentration-UIKit/Concentration-UIKit/ViewController.swift
+++ Concentration-UIKit/Concentration-UIKit/ViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 // UIViewController <- super class
 class ViewController: UIViewController {
     
+    var flipCount: Int = 0
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
     }
     
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 }
 
