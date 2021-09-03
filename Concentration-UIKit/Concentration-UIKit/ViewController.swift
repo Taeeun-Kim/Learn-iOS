a8b3ddf6ef981e8a07eaacf32d93ac73c11e526d
Taeeun Kim <taeeun.kim@outlook.com>
2021-09-03 23:04:09 +0200

    Update flipCount

--- Concentration-UIKit/Concentration-UIKit/ViewController.swift
+++ Concentration-UIKit/Concentration-UIKit/ViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 // UIViewController <- super class
 class ViewController: UIViewController {
     
-    var flipCount: Int = 0
+    var flipCount: Int = 0 {
+        didSet { // observer property
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
         flipCard(withEmoji: "👻", on: sender)
     }
     
     @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCountLabel.text = "Flips: \(flipCount)"
         flipCard(withEmoji: "🎃", on: sender)
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
