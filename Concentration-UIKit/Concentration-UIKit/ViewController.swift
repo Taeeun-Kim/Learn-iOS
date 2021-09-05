cc98ce7f2db879626704d9032e1443c5627f708f
Taeeun Kim <taeeun.kim@outlook.com>
2021-09-05 22:25:18 +0200

    Updata buttons

--- Concentration-UIKit/Concentration-UIKit/ViewController.swift
+++ Concentration-UIKit/Concentration-UIKit/ViewController.swift
@@ -18,21 +18,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var flipCountLabel: UILabel!
     
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        flipCard(withEmoji: "👻", on: sender)
+        
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("error")
+        }
     }
     
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCount += 1
-        flipCountLabel.text = "Flips: \(flipCount)"
-        flipCard(withEmoji: "🎃", on: sender)
-    }
+//    @IBAction func touchSecondCard(_ sender: UIButton) {
+//        flipCount += 1
+//        flipCountLabel.text = "Flips: \(flipCount)"
+//        flipCard(withEmoji: "🎃", on: sender)
+//    }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
