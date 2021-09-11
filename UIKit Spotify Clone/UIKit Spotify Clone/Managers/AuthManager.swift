70d3f3cd80c60f255a8a7acb8c6d57fc731d79d4
Taeeun Kim <taeeun.kim@outlook.com>
2021-09-11 13:46:47 +0200

    update

--- UIKit Spotify Clone/UIKit Spotify Clone/Managers/AuthManager.swift
+++ UIKit Spotify Clone/UIKit Spotify Clone/Managers/AuthManager.swift
@@ -12,8 +12,6 @@ final class AuthManager {
     static let shared = AuthManager()
     
     struct Constants {
-        static let clientId = "4d03d8b9b0024330aac97bb97cccb041"
-        static let clientSecret = "6e4b80c9c2a443cab935c59fe4560c42"
     }
     
     private init() {}
