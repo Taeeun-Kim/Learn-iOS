764f41374f933bc6b0d4e43bfd1ee94ea66c4373
Taeeun Kim <taeeun.kim@outlook.com>
2021-10-29 23:49:01 +0200

    Add AccountSummaryScreen

---
+++ BankApp/BankApp/Screens/AccountSummaryScreen.swift
@@ -0,0 +1,21 @@
+//
+//  AccountSummaryScreen.swift
+//  BankApp
+//
+//  Created by Taeeun Kim on 29.10.21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccountSummaryScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountSummaryScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountSummaryScreen()
+    }
+}
