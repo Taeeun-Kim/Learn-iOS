ece9c6be0fa18a913a4725f0e13414845b28983b
Taeeun Kim <taeeun.kim@outlook.com>
2021-10-30 00:19:22 +0200

    connect with server in Glitch

--- BankApp/BankApp/Screens/AccountSummaryScreen.swift
+++ BankApp/BankApp/Screens/AccountSummaryScreen.swift
@@ -13,11 +13,13 @@ struct AccountSummaryScreen: View {
     @ObservedObject private var accountSummaryVM = AccountSummaryViewModel()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear {
-                self.accountSummaryVM.getAllAccounts()
-            }
-        
+        VStack {
+            AccountListView(accounts: accountSummaryVM.accounts)
+            Text("\(accountSummaryVM.total)")
+        }
+        .onAppear {
+            self.accountSummaryVM.getAllAccounts()
+        }
     }
 }
 
