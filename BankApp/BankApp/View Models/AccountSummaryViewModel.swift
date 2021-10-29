bb3397e1ad575dc7f536de67e3a134b76ba5c852
Taeeun Kim <taeeun.kim@outlook.com>
2021-10-30 00:07:47 +0200

    Connect VM

--- BankApp/BankApp/View Models/AccountSummaryViewModel.swift
+++ BankApp/BankApp/View Models/AccountSummaryViewModel.swift
@@ -10,12 +10,12 @@ import Foundation
 
 class AccountSummaryViewModel: ObservableObject {
     
-    private var _accounts = [Account]()
+    private var _accountModels = [Account]()
     
-    var accounts: [AccountViewModel] = [AccountViewModel]()
+    @Published var accounts: [AccountViewModel] = [AccountViewModel]()
     
     var total: Double {
-        _accounts.map { $0.balance }.reduce(0, +)
+        _accountModels.map { $0.balance }.reduce(0, +)
     }
     
     func getAllAccounts() {
@@ -24,8 +24,10 @@ class AccountSummaryViewModel: ObservableObject {
             switch result {
             case .success(let accounts):
                 if let accounts = accounts {
-                    self._accounts = accounts
-                    self.accounts = accounts.map(AccountViewModel.init)
+                    self._accountModels = accounts
+                    DispatchQueue.main.async {
+                        self.accounts = accounts.map(AccountViewModel.init)
+                    }
                 }
                 
             case .failure(let error):
