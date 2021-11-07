46db9740000a15cea4d44a2669e000712ed8fe4c
Taeeun Kim <taeeun.kim@outlook.com>
2021-11-07 22:06:04 +0100

    Update VM

---
+++ CombineToDoList/CombineToDoList/ToDoFormView.swift
@@ -0,0 +1,60 @@
+//
+//  ToDoFormView.swift
+//  CombineToDoList
+//
+//  Created by Taeeun Kim on 07.11.21.
+//
+
+import SwiftUI
+
+struct ToDoFormView: View {
+    @EnvironmentObject var dataStore: DataStore
+    @ObservedObject var formVM: ToDoFormViewModel
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                VStack(alignment: .leading) {
+                    TextField("ToDo", text: $formVM.name)
+                    Toggle("Completed", isOn: $formVM.completed)
+                }
+            }
+            .navigationTitle("ToDo")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(leading: cancelButton, trailing: updateSaveButton)
+        }
+    }
+}
+
+extension ToDoFormView {
+    func updateToDo() {
+        let toDo = ToDo(id: formVM.id!, name: formVM.name, completed: formVM.completed)
+        dataStore.updateToDo(toDo)
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    func addToDo() {
+        let toDo = ToDo(name: formVM.name)
+        dataStore.addToDo(toDo)
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    var cancelButton: some View {
+        Button("Cancel") {
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    var updateSaveButton: some View {
+        Button( formVM.updating ? "Update" : "Save", action: formVM.updating ? updateToDo : addToDo)
+            .disabled(formVM.updating)
+    }
+}
+
+struct ToDoFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoFormView(formVM: ToDoFormViewModel())
+            .environmentObject(DataStore())
+    }
+}
--- CombineToDoList/CombineToDoList/ToDoFormViewModel.swift
+++ CombineToDoList/CombineToDoList/ToDoFormViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class ToDoFormViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var completed = false
+    var id: String?
+    
+    var updating: Bool {
+        id != nil
+    }
+    
+    var isDisabled: Bool {
+        name.isEmpty
+    }
+    
+    init() {
+        
+    }
+    
+    init(_ currentToDo: ToDo) {
+        self.name = currentToDo.name
+        self.completed = currentToDo.completed
+        id = currentToDo.id
+    }
+}
