5047412024bea1291ccd603665d76200e42aaba3
Taeeun Kim <taeeun.kim@outlook.com>
2021-11-09 09:36:49 +0100

    Init HackingRealityKit

---
+++ HackingRealityKit/HackingRealityKitTests/HackingRealityKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  HackingRealityKitTests.swift
+//  HackingRealityKitTests
+//
+//  Created by Taeeun Kim on 09.11.21.
+//
+
+import XCTest
+@testable import HackingRealityKit
+
+class HackingRealityKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
