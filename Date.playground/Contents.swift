a6560975d517e541c6cc3b5aa07c0068d580c842
Taeeun Kim <taeeun.kim@outlook.com>
2022-09-13 18:55:40 +0200

    Show countries

--- Date.playground/Contents.swift
+++ Date.playground/Contents.swift
@@ -15,33 +15,33 @@ import Foundation
 
 //let date = Date().timeIntervalSinceReferenceDate
 
-
-let past = Date().addingTimeInterval(-50000)
-
-let differentBetweenPastAndNow = -Date().addingTimeInterval(past.timeIntervalSinceNow).timeIntervalSinceNow
-
-let minutes = Int(differentBetweenPastAndNow) / 60 // 초 -> 분
-
-print(minutes)
-
-let hours = minutes / 60 // 분 -> 시간
-let hours2 = minutes % 60 // 분 -> 시간
-
-print(hours)
-print(hours2)
-
-let days = hours / 24 // 시간 -> 일수
-
-print(days)
-
-print("가주아")
-
-let dateFormatter = DateFormatter()
-dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
-dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-
-let dateToString = dateFormatter.string(from: past)
-print(dateToString)
-
-let stringToDate = dateFormatter.date(from: dateToString)
-print(stringToDate!)
+//
+//let past = Date().addingTimeInterval(-50000)
+//
+//let differentBetweenPastAndNow = -Date().addingTimeInterval(past.timeIntervalSinceNow).timeIntervalSinceNow
+//
+//let minutes = Int(differentBetweenPastAndNow) / 60 // 초 -> 분
+//
+//print(minutes)
+//
+//let hours = minutes / 60 // 분 -> 시간
+//let hours2 = minutes % 60 // 분 -> 시간
+//
+//print(hours)
+//print(hours2)
+//
+//let days = hours / 24 // 시간 -> 일수
+//
+//print(days)
+//
+//print("가주아")
+//
+//let dateFormatter = DateFormatter()
+//dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+//dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+//
+//let dateToString = dateFormatter.string(from: past)
+//print(dateToString)
+//
+//let stringToDate = dateFormatter.date(from: dateToString)
+//print(stringToDate!)
---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Taeeun Kim on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
